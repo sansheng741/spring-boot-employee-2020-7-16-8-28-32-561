180483a9476f3bfa3ea6462a496401b0125bf4f7
sansheng741 <807156237@qq.com>
2020-07-28 20:14:54 +0800

    [spring boot employee] [colin&oliver] feature : define interface CompanyService

---
+++ src/main/java/com/thoughtworks/springbootemployee/Service/CompanyService.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.springbootemployee.Service;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+
+import java.util.List;
+
+/**
+ * Created by CHENCO9 on 7/28/2020 8:08 PM
+ */
+public interface CompanyService {
+    List<Company> queryAllCompany();
+
+    Company queryCompanyById(Integer id);
+
+    List<Employee> queryEmpByCompanyId(Integer id);
+
+    List<Company> pageQueryCompany(int page,int pageSize);
+
+    void addCompany(Company company);
+
+    void updateComanyInfo(Integer id, Company company);
+
+    void delCompany(Integer id);
+}
