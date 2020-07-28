484991cc6a69607c87b346d766531bc545334b45
sansheng741 <807156237@qq.com>
2020-07-28 20:03:41 +0800

    [spring boot employee] [colin&oliver] bug-fix:implement query page and query by gender

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -17,32 +17,39 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping("/employees")
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeService.selectAllEmployees();
     }
 
     @GetMapping("/employees/{id}")
-    public Employee getEmployees(@PathVariable("id") Integer id){
+    public Employee getEmployees(@PathVariable("id") Integer id) {
         return employeeService.selectEmployeeById(id);
     }
 
     @PostMapping("/employees")
-    public void addEmployees(Employee employee){
+    public void addEmployees(Employee employee) {
         employeeService.addEmployee(employee);
     }
 
     @DeleteMapping("/employees/{id}")
-    public void deleteEmployees(@PathVariable("id") Integer id){
+    public void deleteEmployees(@PathVariable("id") Integer id) {
         employeeService.delEmployeeById(id);
     }
 
     @PutMapping("/employees/{id}")
-    public void updateEmployees(@PathVariable("id") Integer id,Employee employee){
+    public void updateEmployees(@PathVariable("id") Integer id, Employee employee) {
         employeeService.updateEmployeeById(id, employee);
     }
 
+    @GetMapping("/employees/{page}/{pageSize}")
+    public List<Employee> pageQueryEmployee(@PathVariable("page") Integer page, @PathVariable("pageSize") Integer pageSize) {
+        return employeeService.pageQueryEmployee(page, pageSize);
+    }
 
-
+    @GetMapping("/employees/gender={gender}")
+    public List<Employee> queryEmployeesByGender(@PathVariable("gender") String gender) {
+        return employeeService.selectEmployeeByGender(gender);
+    }
 
 
 }
